90508334943dc8ee37d4c79c1e6eaf7ccd520da9
Nikita <fomin.nv@phystech.edu>
2024-06-17 17:53:00 +0300

    showing number of cols and rows of each table

--- app/Http/Controllers/Import/IndexController.php
+++ app/Http/Controllers/Import/IndexController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Import;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke() {
-        return view("import.main");
+        $tables_names = DB::select("SHOW TABLES");
+        $tables = new \ArrayObject([]);
+        foreach ($tables_names as $table_name) {
+            eval('$table_name = $table_name->Tables_in_' . env('DB_DATABASE') . ';');
+            $tables->append([$table_name, DB::table($table_name)->count(), count(\Schema::getColumnListing($table_name))]);
+        }
+
+        return view("import.main", ["tables"=> $tables]);
     }
 }
--- resources/views/import/main.blade.php
+++ resources/views/import/main.blade.php
@@ -17,7 +17,26 @@
       <section class="content">
         <div class="container-fluid">
           <div class="row">
-              123
+            @foreach ($tables as $table)                
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <h4>
+                        {{ $table[0] }}
+                    </h4>
+                  </div>
+                  <h3 class="ml-2">
+                    Rows: {{ $table[1] }} <br>
+                    Columns: {{ $table[2] }}
+                  </h3>
+                  <div class="icon">
+                    <i class="fa fa-solid fa-database"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            @endforeach
         </div>
       </section>
       <!-- /.content -->
