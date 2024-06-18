5a5c96ae08711d1a7aa6b44a4743a7b43b1fc487
Nikita <fomin.nv@phystech.edu>
2024-06-18 03:12:54 +0300

    Orders

---
+++ database/migrations/2024_06_18_001031_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('g_number');
+            $table->dateTime('date');
+            $table->dateTime('last_change_date');
+            $table->string('supplier_article');
+            $table->string('tech_size');
+            $table->string('barcode');
+            $table->unsignedBigInteger('total_price');
+            $table->unsignedBigInteger('discount_percent');
+            $table->string('warehouse_name');
+            $table->string('oblast');
+            $table->unsignedBigInteger('income_id');
+            $table->string('odid')->nullable();
+            $table->unsignedBigInteger('nm_id');
+            $table->string('subject');
+            $table->string('category');
+            $table->string('brand');
+            $table->boolean('is_cancel');
+            $table->string('cancel_dt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
